c6da25f305d91d5965a925de0ab7ac35953f8714
wonkatin <j.drawers@gmail.com>
2021-02-05 22:29:33 -0600

    Initial Commit: Pixesl fill the screen, no pixel function, no button, yet

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // The magic happens here!
+/* Constants */
+
+/* Game Logic Variables and State */
+let divCount = 0
+// let enterEventCount = 0;
+// let leaveEventCount = 0;
+/* DOM References */
+const pixelTarget = document.querySelector(".pixel");
+const screenTarget = document.querySelector("#screen")
+/* Functions and Game Logic */
+
+const createDiv = () => {
+   const pix = document.createElement("div");
+   pix.classList.add("pixel");
+   screenTarget.append(pix);
+
+};
+const fillScreen = () =>{
+    for (i = 0; i < 9000; i++) {
+        createDiv();
+        divCount++;
+        //console.log(divCount);
+    }
+};
+fillScreen()
+/* Event Listeners */
+screenTarget.addEventListener("mouseenter", function(){
+   for (i = 0; i < divCount; i++) {
+   pixelTarget.classList.add("colored-in") 
+}
+});
+
+
+// class Pixel {
+//    constructor(pix) {
+//      this.pix = pix
+//      this.addIt = function(){
+//         this.pix = document.createElement("div");
+//         pix.classList.add("pixel");
+//         screenTarget.append(pix);
+//      }
+//    }
+// }
