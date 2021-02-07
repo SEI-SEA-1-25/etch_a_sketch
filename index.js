a6343e5929b75b9aed5ed34761142d9e94667bb5
kchu01 <chukathy01@gmail.com>
2021-02-06 16:13:11 -0800

    Add Edit 1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// The magic happens here!
+
+/* DOM References */
+let container = document.querySelector('.container')
+let pixel = document.querySelector('.pixel')
+/* Functions and Game Logic */
+
+// changes color of pixel when mouse enters
+pixel.addEventListener('mouseenter', entering);
+
+function entering(e) {
+    pixel.classList.add('colored-in');
+    e.currentTarget.style.background = '';
+    console.log('mouseenter');
+}
+
+/* Event Listeners */
+// document.addEventListener('DOMContentLoaded', start);
+// document.addEventListener('clear-screen-button', clear);
+
