e258e830fdc6b8529f98186579a54e9c9fb97dd3
kchu01 <chukathy01@gmail.com>
2021-02-06 17:40:38 -0800

    Add Edit 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 /* DOM References */
 let container = document.querySelector('.container')
 let pixel = document.querySelector('.pixel')
+let screen = document.querySelector('#screen')
 /* Functions and Game Logic */
 
 // changes color of pixel when mouse enters
@@ -13,7 +14,35 @@ function entering(e) {
     console.log('mouseenter');
 }
 
+// fill screen with pixels
+
+function fillScreen() {
+    // e.preventDefault();
+
+    for (let i = 0; i < 10000; i++) {
+        // let filledPixel = document.createElement('filledPixel')
+        // let pixels = document.createElement('div')
+        // pixels. = pixel
+
+        // // filledPixel.classList.add('filledPixel');
+        // // screen.appendChild(filledPixel);
+        // filledPixel.appendchild(pixel)
+
+
+        // return filledPixel;
+
+        let div = document.createElement('div');
+        let childPixel = document.createElement('pixel');
+        div.appendChild(childPixel)
+
+        screen.append(div)
+
+    }
+
+}
+
+
 /* Event Listeners */
-// document.addEventListener('DOMContentLoaded', start);
+document.addEventListener('DOMContentLoaded', fillScreen);
 // document.addEventListener('clear-screen-button', clear);
 
