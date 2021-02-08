b051cc9b95994da9eb9ebca9fa1f9f88e3b79f4a
ChrisB007 <chrisbitoy@gmail.com>
2021-02-08 13:02:38 -0500

    pixels are added dynamically (small progress)

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// The magic happens here!
+// Seecting the DOM
+let screen = document.querySelector("#screen");
+let pixel = document.querySelector("#onepix");
+
+
+
+
+
+
+// EventListeners
+
+//Change bg color of one pixel
+pixel.addEventListener("mouseenter", ()=> {
+    pixel.classList.add("colored-in");
+
+    //Create a new div dynamically
+    var newDiv = document.createElement("div");
+    
+    //Add ID of 'onepix' and a CLASS of 'pixel'
+    newDiv.setAttribute("class", "pixel");
+
+    //Append div to 'screen' div.
+    screen.append(newDiv);
+    console.log(newDiv);
+    
+    for (var i = 0; i < screen.length; i++){
+        
+    }
+});
+
+
+
+
+
+
+
+
+//Functions
