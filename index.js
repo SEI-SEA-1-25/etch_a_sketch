ec259035c42225bc7d27cc5c3d548d38dbb9d9d5
mariiamel <mariiamelnikova96@gmail.com>
2021-02-08 09:00:54 -0800

    Etch HW

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// The magic happens here!
+console.log('hey there');
+
+
+/* DOM References */
+let pixel = document.querySelector('.pixel');
+let clearBut = document.getElementById('clear-screen-button');
+let screen = document.getElementById('screen');
+
+/* Functions and Game Logic */
+
+function clearFunction() {
+    for (div of screen.children) {
+        div.classList.remove('colored-in')
+    }
+}
+
+function addPixels() {
+ for (let i = 0; i < 3000; i++) {
+     let allDiv = document.createElement('div');
+     let allPixels = document.createElement('pixel');
+     allDiv.appendChild(allPixels);
+     screen.appendChild(allDiv);
+    i++
+ }
+}
+
+/* Event Listeners */
+pixel.addEventListener('mouseenter', e => {
+    console.log(e.target)
+    e.target.classList.add('colored-in');
+})
+
+clearBut.addEventListener('click', clearFunction)
+document.addEventListener('DOMContentLoaded', addPixels)
