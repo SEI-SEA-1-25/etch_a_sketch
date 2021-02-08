67f7b6cb86583f736030002fc53ad633081b6d0e
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-08 08:34:30 -0800

    trying the requested way still

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
 window.addEventListener("load", function () {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
+  const screen = document.querySelector("#screen");
 
-  // EventListeners
-  canvas.addEventListener("mousedown", startPosition);
-  canvas.addEventListener("mouseup", finishedPosition);
-  canvas.addEventListener("mousemove", draw);
+  const drawSpace = document.querySelector("#screen");
+  const resetButton = document.querySelector(".reset");
+  const cloneColumns = document.querySelector(".pixels");
 
-  //resize
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+  const rows = document.querySelector(".rows");
+  const columns = document.querySelector(".columns");
 
-  // ctx.beginPath();
-  // ctx.moveTo(100, 100);
-  // ctx.lineTo(100, 100);
-  // ctx.lineTo(100, 100);
-  // ctx.closePath();
-  // ctx.stroke();
-  // ctx.strokeStyle = "red";
-  // ctx.lineWidth = 5;
-  // ctx.strokeRect(50, 200, 500, 500);
+  const newCol = Object.assign({ columns });
+  console.log(newCol);
 
-  //variables
-  let painting = false;
-
-  //functions
-  function startPosition(e) {
-    painting = true;
-    draw(e);
-  }
-  function finishedPosition() {
-    painting = false;
-    ctx.beginPath();
-  }
-  
-  function draw(e) {
-    if (!painting) {
-      console.log("crap!!");
-    } else {
-      ctx.lineWidth = 10;
-      ctx.lineCap = "round";
-      ctx.lineTo(e.clientX, e.clientY);
-      ctx.stroke();
-      ctx.beginPath();
-      ctx.moveTo(e.clientX, e.clientY);
-    }
-  }
+  const newRow = Object.assign({ rows });
+  console.log(newRow);
 });
