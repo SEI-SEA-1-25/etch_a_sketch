389dd420b240a1c44d16b19241a0bba323f78ab1
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-08 01:11:16 -0800

    got it drawing
    needs button x,y draw instead of mouse events

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// The magic happens here!
+window.addEventListener("load", function () {
+  const canvas = document.querySelector("#canvas");
+  const ctx = canvas.getContext("2d");
+
+  // EventListeners
+  canvas.addEventListener("mousedown", startPosition);
+  canvas.addEventListener("mouseup", finishedPosition);
+  canvas.addEventListener("mousemove", draw);
+
+  //resize
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  // ctx.beginPath();
+  // ctx.moveTo(100, 100);
+  // ctx.lineTo(100, 100);
+  // ctx.lineTo(100, 100);
+  // ctx.closePath();
+  // ctx.stroke();
+  // ctx.strokeStyle = "red";
+  // ctx.lineWidth = 5;
+  // ctx.strokeRect(50, 200, 500, 500);
+
+  //variables
+  let painting = false;
+
+  //functions
+  function startPosition(e) {
+    painting = true;
+    draw(e);
+  }
+  function finishedPosition() {
+    painting = false;
+    ctx.beginPath();
+  }
+  
+  function draw(e) {
+    if (!painting) {
+      console.log("crap!!");
+    } else {
+      ctx.lineWidth = 10;
+      ctx.lineCap = "round";
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX, e.clientY);
+    }
+  }
+});
