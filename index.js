e12cf4ab15d4a1fad1e6871c4a779a00d5ce37ef
ianalexanderz <ianstewart745@gmail.com>
2021-02-08 20:05:31 -0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // The magic happens here!
+let pixel = document.querySelector('.pixel')
+
+
+
+
+
+
+const update = (event) => {
+   pixel.style.backgroundColor = 'black';
+
+}
+
+const update2 = (event) => {
+    pixel.style.backgroundColor = 'white';
+}
+
+
+document.querySelector('.pixel').addEventListener('DOMContentLoaded', buildGrid)
+document.querySelector('.pixel').addEventListener('mouseenter', update)
+document.querySelector('.pixel').addEventListener('mouseleave', update2)
+
+
+
+var buildGrid = function(x) {
+    var squareSize = (scrnwidth/x);
+    $('.square').remove();
+  
+    for(var i = 0; i < (x*x)*(scrnheight/scrnwidth) ; i++) {
+      $('#grid').append("<div class='square'></div>")
+    }
+  
+    $('.square').width(squareSize);
+    $('.square').height(squareSize);
+  };
