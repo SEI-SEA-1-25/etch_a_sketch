28fb5644f3bc3088e2a0ae2597589ed1fbceea12
the                                                            Z giant <zionagrate@gmail.com>
2022-08-18 07:05:19 -0400

    All done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// The magic happens here!
+document.addEventListener("DOMContentLoaded", () => {
+    let screen = document.querySelector("#screen")
+    let clearScreen = document.querySelector("#clear-screen-button")
+
+    let mouseHover = (e) => {
+        e.target.classList.add("colored-in")
+    }  
+
+    let etchSketch = () => {
+        for (let i = 0; i < 9000; i++) {
+            let sketch = document.createElement("div")
+            sketch.classList.add("pixel")
+            sketch.addEventListener("mouseover", mouseHover)
+            screen.append(sketch)
+           }
+    }
+    etchSketch()
+
+
+
+    clearScreen.addEventListener("click", () => {
+        while(screen.firstChild) {
+            screen.removeChild(screen.firstChild)
+        }
+        for (let i = 0; i < 9000; i++) {
+            let sketch = document.createElement("div")
+            sketch.classList.add("pixel")
+            sketch.addEventListener("mouseover", mouseHover)
+            screen.append(sketch)
+    }
+})
+})
