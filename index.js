8edbb85f5e63115139e4655a92fdc3f164a23c30
SClemson49 <s.clemson49@gmail.com>
2022-11-28 20:38:39 -0900

    etchy sketchy

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // The magic happens here!
+
+const pixel = document.querySelector('.pixel')
+const screen = document.querySelector('#screen')
+const clearButton = document.querySelector('#clear-screen-button')
+
+
+pixel.addEventListener('mouseenter', (e) => {
+    e.target.classList.add('colored-in')
+})
+
+
+
+for (let i = 0; i < 7990; i++) {
+    const newPix = document.createElement('div')
+    newPix.classList.add('pixel')
+    newPix.addEventListener('mouseenter', (e) => {
+        e.target.classList.add('colored-in')
+         })
+         screen.appendChild(newPix)}
+
+clearButton.addEventListener('click', ()=> {
+    const allPixel = document.querySelectorAll('.pixel')
+    allPixel.forEach(pixels => {
+        pixels.classList.remove('colored-in')
+    })
+})
